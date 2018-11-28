61fe0f2470c1709b7adb6cfc8ef50163712ddfce
Haerul Muttaqin <haerul.muttaqin@gmail.com>
2018-11-29 05:26:53 +0700

    add click listener

--- app/src/main/java/com/haerul/swipeviewpager/Adapter.java
+++ app/src/main/java/com/haerul/swipeviewpager/Adapter.java
@@ -1,6 +1,7 @@
 package com.haerul.swipeviewpager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
@@ -34,7 +35,7 @@ public class Adapter extends PagerAdapter {
 
     @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
         layoutInflater = LayoutInflater.from(context);
         View view = layoutInflater.inflate(R.layout.item, container, false);
 
@@ -48,6 +49,16 @@ public class Adapter extends PagerAdapter {
         imageView.setImageResource(models.get(position).getImage());
         title.setText(models.get(position).getTitle());
         desc.setText(models.get(position).getDesc());
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("param", models.get(position).getTitle());
+                context.startActivity(intent);
+                // finish();
+            }
+        });
 
         container.addView(view, 0);
         return view;
